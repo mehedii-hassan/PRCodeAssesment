cb17b723f21647f298182238335a5ce4521fafc9
Mehedi-Hassan <mehedii.hassan01@gmail.com>
2022-06-17 23:43:14 +0600

    some changes

--- app/src/main/java/com/example/androidassesment/ui/LoginPage.java
+++ app/src/main/java/com/example/androidassesment/ui/LoginPage.java
@@ -2,17 +2,21 @@ package com.example.androidassesment.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.androidassesment.R;
+import com.example.androidassesment.databinding.FragmentLoginPageBinding;
 
 public class LoginPage extends Fragment {
 
-
+    private FragmentLoginPageBinding binding;
 
     public LoginPage() {
         // Required empty public constructor
@@ -22,7 +26,17 @@ public class LoginPage extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_login_page, container, false);
+        binding = FragmentLoginPageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        binding.btnSubmitLP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_loginPage_to_googleMap);
+            }
+        });
     }
 }
--- app/src/main/java/com/example/androidassesment/ui/RegistrationPage.java
+++ app/src/main/java/com/example/androidassesment/ui/RegistrationPage.java
@@ -2,28 +2,43 @@ package com.example.androidassesment.ui;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.example.androidassesment.R;
+import com.example.androidassesment.databinding.FragmentRegistrationPageBinding;
 
 public class RegistrationPage extends Fragment {
 
 
+    private FragmentRegistrationPageBinding binding;
 
     public RegistrationPage() {
         // Required empty public constructor
     }
 
 
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_registration_page, container, false);
+        binding = FragmentRegistrationPageBinding.inflate(inflater, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+
+        binding.btnSubmitRP.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Navigation.findNavController(view).navigate(R.id.action_registrationPage_to_loginPage);
+            }
+        });
     }
 }
