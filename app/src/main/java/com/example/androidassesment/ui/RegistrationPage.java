27f667ff17a4346f03c1374cebd1c28865e356c8
Mehedi-Hassan <mehedii.hassan01@gmail.com>
2022-06-17 10:09:22 +0600

    registration page design complete

---
+++ app/src/main/java/com/example/androidassesment/ui/RegistrationPage.java
@@ -0,0 +1,29 @@
+package com.example.androidassesment.ui;
+
+import android.os.Bundle;
+
+import androidx.fragment.app.Fragment;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.example.androidassesment.R;
+
+public class RegistrationPage extends Fragment {
+
+
+
+    public RegistrationPage() {
+        // Required empty public constructor
+    }
+
+
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        // Inflate the layout for this fragment
+        return inflater.inflate(R.layout.fragment_registration_page, container, false);
+    }
+}
